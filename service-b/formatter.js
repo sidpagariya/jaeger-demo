900bea454c6af8825ce28208d74f3d5a79ad11f3
Sid Pagariya <sid.pagariya@ithaka.org>
2020-06-25 11:02:57 -0400

    POW-1040: Add a third service

--- service-b/formatter.js
+++ service-b/formatter.js
@@ -12,4 +12,25 @@ function formatGreeting(req, res) {
   span.finish()
   res.send(response)
 }
-module.exports = formatGreeting
+
+const bent = require('bent')
+
+async function formatGreetingRemote(req, res) {
+  const span = tracer.startSpan('format-greeting-remote', { childOf: req.span })
+  // Parse the handler input
+  const name = req.query.name
+  span.log({ event: 'format', message: `formatting message remotely for name ${name}` })
+  const service = process.env.SERVICE_FORMATTER || 'localhost'
+  const servicePort = process.env.SERVICE_FORMATTER_PORT || '8082'
+  const url = `http://${service}:${servicePort}/formatGreeting?name=${name}`
+  console.log(url)
+  const headers = {}
+  tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, headers)
+  const request = bent('string', headers)
+  const response = await request(url)
+  const baggage = span.getBaggageItem('my-baggage')
+  span.log({ event: 'baggage', message: `this is baggage ${baggage}` })
+  span.finish()
+  res.send(response)
+}
+module.exports = formatGreetingRemote
